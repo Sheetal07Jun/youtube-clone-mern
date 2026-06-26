86e0ee5de0ba8376e9c75464103be8542f965e36
Sheetal07Jun <yadavsheetal9927@gmail.com>
2026-06-26 15:27:24 +0530

    Did SOme Chnages in History page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -30,32 +30,21 @@ function History() {
     };
 
     return (
-        <div style={{ padding: "20px" }}>
+        <div className="history-page">
             <h1>Watch History</h1>
 
             {history.length === 0 ? (
                 <p>No videos watched yet.</p>
             ) : (
                 history.map((item) => (
-                    <div
-                        key={item._id}
-                        style={{
-                            marginBottom: "20px",
-                        }}
-                    >
+                    <div key={item._id} className="history-card">
                         <Link
                             to={`/video/${item.videoId?._id}`}
                         >
                             <img
-                                src={
-                                    item.videoId
-                                        ?.thumbnailUrl
-                                }
-                                alt={
-                                    item.videoId
-                                        ?.title
-                                }
-                                width="250"
+                                src={video.thumbnailUrl}
+                                alt={video.title}
+                                className="history-thumbnail"
                             />
                         </Link>
 
